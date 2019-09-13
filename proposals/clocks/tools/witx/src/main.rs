b771ea33838ac6e9aa6fb59232d62e6fbc68545c
Pat Hickey <pat@moreproductive.org>
2019-09-13 10:27:21 -0700

    Add witx crate, to parse and validate witx (#90)
    * import witx-frontend crate
    
    * witx-frontend: add test that wasi_unstable.witx validates
    
    * witx-frontend: better error reporting
    
    * witx-frontend: add witx-specific pointer and constpointer constructions
    
    * wasi_unstable: fix syntax of preopentype_t
    
    * witx-frontend: add LICENSE file, fix repo capitalization
    
    * rename witx-frontend to witx

---
+++ proposals/clocks/tools/witx/src/main.rs
@@ -0,0 +1,38 @@
+use clap::{App, Arg};
+use std::path::Path;
+use std::process;
+use witx::load;
+
+pub fn main() {
+    let app = App::new("witx")
+        .version(env!("CARGO_PKG_VERSION"))
+        .about("Validate witx file format")
+        .arg(
+            Arg::with_name("input")
+                .required(true)
+                .help("path to root of witx document"),
+        )
+        .arg(
+            Arg::with_name("verbose")
+                .short("v")
+                .long("verbose")
+                .takes_value(false)
+                .required(false),
+        )
+        .get_matches();
+
+    match load(Path::new(app.value_of("input").expect("required arg"))) {
+        Ok(doc) => {
+            if app.is_present("verbose") {
+                println!("{:?}", doc)
+            }
+        }
+        Err(e) => {
+            println!("{}", e.report());
+            if app.is_present("verbose") {
+                println!("{:?}", e);
+            }
+            process::exit(1)
+        }
+    }
+}
