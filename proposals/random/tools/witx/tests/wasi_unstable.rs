96e8123a2ea0aa56c22e5ec141cbb5c5e23c2b71
Dan Gohman <sunfish@mozilla.com>
2019-09-23 15:08:37 -0700

    Fix the name of `wasi_ephemeral_preview0` and update tests. (#92)
    `wasi_ephemeral_preview0` is meant to be versioned, so that it can be
    superceded by new versions in the future.
    
    Also, update the witx testsuite to test all the current witx files.

--- proposals/random/tools/witx/tests/wasi_unstable.rs
+++ proposals/random/tools/witx/tests/wasi_unstable.rs
@@ -1,9 +1,23 @@
 use std::path::Path;
+use witx;
 
 #[test]
-fn validate_wasi_unstable() {
+fn validate_wasi_unstable_preview0() {
     witx::load(Path::new(
         "../../phases/unstable/witx/wasi_unstable_preview0.witx",
     ))
     .unwrap();
+}
+
+#[test]
+fn validate_wasi_ephemeral_preview0() {
+    witx::load(Path::new(
+        "../../phases/ephemeral/witx/wasi_ephemeral_preview0.witx",
+    ))
+    .unwrap();
+}
+
+#[test]
+fn validate_wasi_old_preview0() {
+    witx::load(Path::new("../../phases/old/witx/wasi_unstable.witx")).unwrap();
 }
