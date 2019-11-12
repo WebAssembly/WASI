a50c9c9cf2ef21f25ffb6984531a3bcb2d317bce
Pat Hickey <phickey@fastly.com>
2019-11-11 16:10:15 -0800

    Create snapshot 1 (#147)
    * archive: contents of `old` moved underneath `snapshot_0`
    
    The contents of `old` are  identical to the current
    `unstable/wasi_unstable_preview0` in every sense except the name of the
    module and file.
    
    So, for this snapshotting, rather than move the contents of the current
    "unstable" (which will be renamed "snapshot" as part of this PR) under a
    new directory under phases/old, we're just moving the current contents
    under a directory, so that future snapshots can be archived without
    changing these paths.
    
    * delete current unstable (snapshot), rather than move it under old.
    
    Special case of this first-time snapshotting process.
    
    * copy phases/ephemeral/* into phases/snapshot/
    
    * rename files in snapshot to snapshot_preview1
    
    * update phases/README to rename `unstable` to `snapshot`
    
    Per https://github.com/WebAssembly/WASI/issues/138 and discussion on
    07nov19 wasi video call.
    
    Updates the steps for PR based on what I did for this first snapshot.
    
    * witx: tests point at newly snapshotted and archived witx specs
    
    and the test file gets renamed to just regular wasi.rs
    
    * fixes in phases readme per dan's code review

--- proposals/filesystem/tools/witx/tests/wasi.rs
+++ proposals/filesystem/tools/witx/tests/wasi.rs
@@ -2,15 +2,15 @@ use std::path::Path;
 use witx;
 
 #[test]
-fn validate_wasi_unstable_preview0() {
+fn validate_wasi_snapshot() {
     witx::load(Path::new(
-        "../../phases/unstable/witx/wasi_unstable_preview0.witx",
+        "../../phases/snapshot/witx/wasi_snapshot_preview1.witx",
     ))
     .unwrap_or_else(|e| panic!("failed to parse: {}", e));
 }
 
 #[test]
-fn validate_wasi_ephemeral_preview() {
+fn validate_wasi_ephemeral() {
     witx::load(Path::new(
         "../../phases/ephemeral/witx/wasi_ephemeral_preview.witx",
     ))
@@ -18,15 +18,17 @@ fn validate_wasi_ephemeral_preview() {
 }
 
 #[test]
-fn validate_wasi_old_preview0() {
-    witx::load(Path::new("../../phases/old/witx/wasi_unstable.witx"))
-        .unwrap_or_else(|e| panic!("failed to parse: {}", e));
+fn validate_wasi_old_snapshot_0() {
+    witx::load(Path::new(
+        "../../phases/old/snapshot_0/witx/wasi_unstable.witx",
+    ))
+    .unwrap_or_else(|e| panic!("failed to parse: {}", e));
 }
 
 #[test]
 fn render_roundtrip() {
     let doc = witx::load(Path::new(
-        "../../phases/unstable/witx/wasi_unstable_preview0.witx",
+        "../../phases/snapshot/witx/wasi_snapshot_preview1.witx",
     ))
     .unwrap_or_else(|e| panic!("failed to parse: {}", e));
 
