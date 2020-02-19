332bcbdb0c4f736881bdc365696e86e8ce860dca
Pat Hickey <pat@moreproductive.org>
2020-02-18 16:35:06 -0800

    witx: export UnionLayout type

--- tools/witx/src/lib.rs
+++ tools/witx/src/lib.rs
@@ -33,7 +33,7 @@ pub use ast::{
 pub use coretypes::{AtomType, CoreFuncType, CoreParamSignifies, CoreParamType, TypePassedBy};
 pub use docs::Documentation;
 pub use io::{Filesystem, MockFs, WitxIo};
-pub use layout::{Layout, SizeAlign, StructMemberLayout};
+pub use layout::{Layout, SizeAlign, StructMemberLayout, UnionLayout};
 pub use parser::DeclSyntax;
 pub use render::SExpr;
 pub use representation::{RepEquality, Representable};
