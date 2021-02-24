682e0e8c2a69c29ac1bd9b0e25d963683416f21c
Pat Hickey <pat@moreproductive.org>
2021-02-23 16:15:15 -0800

    add ephemeral

--- tools/repo_docs.sh
+++ tools/repo_docs.sh
@@ -1,3 +1,15 @@
 cd $(dirname $(realpath $0))/witx
 cargo run -p witx-cli -- docs ../../phases/snapshot/witx/wasi_snapshot_preview1.witx --output ../../phases/snapshot/docs.md
 cargo run -p witx-cli -- docs ../../phases/old/snapshot_0/witx/wasi_unstable.witx --output ../../phases/old/snapshot_0/docs.md
+cargo run -p witx-cli -- docs \
+  ../../phases/ephemeral/witx/wasi_ephemeral_args.witx \
+  ../../phases/ephemeral/witx/wasi_ephemeral_clock.witx \
+  ../../phases/ephemeral/witx/wasi_ephemeral_environ.witx \
+  ../../phases/ephemeral/witx/wasi_ephemeral_fd.witx \
+  ../../phases/ephemeral/witx/wasi_ephemeral_path.witx \
+  ../../phases/ephemeral/witx/wasi_ephemeral_poll.witx \
+  ../../phases/ephemeral/witx/wasi_ephemeral_proc.witx \
+  ../../phases/ephemeral/witx/wasi_ephemeral_random.witx \
+  ../../phases/ephemeral/witx/wasi_ephemeral_sched.witx \
+  ../../phases/ephemeral/witx/wasi_ephemeral_sock.witx \
+  --output ../../phases/ephemeral/docs.md
