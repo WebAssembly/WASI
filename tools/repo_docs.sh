5ab83a68d4eb4f218a898ed03b963b7393caaedc
katelyn martin <kmartin@fastly.com>
2021-06-23 17:16:56 -0400

    Remove `witx` workspace (#438)
    * move 'tools/witx/cli' crate into 'tools/witx-cli'
    
    * update witx and witx-cli manifests
    
    * update repo doc script

--- tools/repo_docs.sh
+++ tools/repo_docs.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -ex
-cd $(dirname $(realpath $0))/witx
-cargo run -p witx-cli -- docs $1 ../../phases/snapshot/witx/wasi_snapshot_preview1.witx --output ../../phases/snapshot/docs.md
-cargo run -p witx-cli -- docs $1 ../../phases/old/snapshot_0/witx/wasi_unstable.witx --output ../../phases/old/snapshot_0/docs.md
-cargo run -p witx-cli -- docs $1 \
+cd $(dirname $(realpath $0))/witx-cli
+cargo run -- docs $1 ../../phases/snapshot/witx/wasi_snapshot_preview1.witx --output ../../phases/snapshot/docs.md
+cargo run -- docs $1 ../../phases/old/snapshot_0/witx/wasi_unstable.witx --output ../../phases/old/snapshot_0/docs.md
+cargo run -- docs $1 \
   ../../phases/ephemeral/witx/wasi_ephemeral_args.witx \
   ../../phases/ephemeral/witx/wasi_ephemeral_clock.witx \
   ../../phases/ephemeral/witx/wasi_ephemeral_environ.witx \
